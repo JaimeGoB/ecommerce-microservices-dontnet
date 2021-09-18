50d5784dc990f962cf47f76fec1b645fc886ea09
JaimeGoB <josba93@gmail.com>
2021-09-18 18:13:48 -0500

    Created Product repository abstraction and implementation with rest verbs.

--- src/Services/Catalog/Catalog.API/Repositories/IProductRepository.cs
+++ src/Services/Catalog/Catalog.API/Repositories/IProductRepository.cs
@@ -6,13 +6,19 @@ namespace Catalog.API.Repositories
 {
     public interface IProductRepository
     {
+        /*GET*/
         Task<IEnumerable<Product>> GetProducts();
         Task<Product> GetProduct(string id);
         Task<IEnumerable<Product>> GetProductByName(string name);
         Task<IEnumerable<Product>> GetProductByCategory(string categoryName);
-
+        
+        /*POST*/
         Task CreateProduct(Product product);
+        
+        /*PUT*/
         Task<bool> UpdateProduct(Product product);
+
+        /*DELETE */
         Task<bool> DeleteProduct(string id);
     }
 }
